9e395191fffa3377e89d96a3912a9f4d951114a0
Degephomk <degiatb.k@gmail.com>
2023-05-15 13:26:56 +0000

    adding

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,11 @@ Write a function named index_range that takes two integer
 """
 
 def index_range(page: int, page_size: int) -> tuple:
+
+    """
+    Write a function named index_range that takes two integer arguments page 
+    function should return a tuple of size two containing a start index 
+    """
     
     index_tuple = page_size * (page - 1), page * page_size
     return index_tuple
